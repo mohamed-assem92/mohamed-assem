d55ca2209075394dc5ca1cc24648054a3828bc2d
mohamed-assem92 <mohamed.abdullatief92@gmail.com>
2018-04-05 03:42:32 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,25 @@ jQuery(function ($) {
 
     (function () {
         $('#preloader').delay(200).fadeOut('slow');
+
+        $("#contactForm").submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+              type: "POST",
+              url: "sendemail.php",
+              data: $("#contactForm").serialize(),
+              success: function(data)
+              {
+                console.log("success");
+                console.log(data);
+              },error: function (err) {
+                console.log("error");
+                console.log(err);
+              }
+            });
+  
+          });
+
     }());
 
 
