3683a3a94061aa1091295f7f0fff5eec10948d67
mohamed-assem92 <mohamed.abdullatief92@gmail.com>
2018-04-05 04:31:38 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -9,23 +9,23 @@ jQuery(function ($) {
     (function () {
         $('#preloader').delay(200).fadeOut('slow');
 
-        $("#contactForm").submit(function(e) {
-            e.preventDefault();
-            $.ajax({
-              type: "POST",
-              url: "sendemail.php",
-              data: $("#contactForm").serialize(),
-              success: function(data)
-              {
-                console.log("success");
-                console.log(data);
-              },error: function (err) {
-                console.log("error");
-                console.log(err);
-              }
-            });
+        // $("#contactForm").submit(function(e) {
+        //     e.preventDefault();
+        //     $.ajax({
+        //       type: "POST",
+        //       url: "sendemail.php",
+        //       data: $("#contactForm").serialize(),
+        //       success: function(data)
+        //       {
+        //         console.log("success");
+        //         console.log(data);
+        //       },error: function (err) {
+        //         console.log("error");
+        //         console.log(err);
+        //       }
+        //     });
   
-          });
+        //   });
 
     }());
 
